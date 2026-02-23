a539ef115f470bc331b9eba2309782e21eb63d2d
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2026-02-23 01:06:07 -0800

    gptel-parse-tool-call: Add tests for gptel--inject-tool-args
    * gptel-parse-tool-call.el (gptel-test-inject-tool-args):  Add
    unit tests for `gptel--inject-tool-args', meant for modifying the
    tool call arguments supplied by LLMs.

--- gptel-parse-tool-call.el
+++ gptel-parse-tool-call.el
@@ -6,6 +6,124 @@
 (require 'cl-lib)
 (require 'map)
 
+;; Unit tests for `gptel--inject-tool-args'
+(ert-deftest gptel-test-inject-tool-args ()
+  "Ensure that tool argument injection into messages arrays works for all backends."
+  (skip-unless (fboundp 'gptel--inject-tool-args))
+  (let* ((backend (alist-get 'openai gptel-test-backends))
+         (testinfo
+          `( :backend ,backend
+             :data (:messages
+                    [( :role "system" :content
+                       "You are a large language model living in Emacs and a helpful assistant.")
+                     ( :role "user" :content
+                       [(:type "text" :text "what are the reviews like for Hamlet (1996)?")])
+                     ( :role "assistant" :content :null :tool_calls
+                       [( :id "fc_bb3ceed0-6c06-49ca-8851-8bde396c85aa" :type "function"
+                          :function ( :name "WebSearch"
+                                      :arguments "{\"count\":5,\"query\":\"Hamlet 1996 reviews\"}")
+                          :index 0)]
+                       :reasoning
+                       "We need up-to-date info about reviews for the film Hamlet (1996). \
+We need to search the web. Use functions.WebSearch.")])))
+         (tool-call '( :id "fc_bb3ceed0-6c06-49ca-8851-8bde396c85aa" :name "WebSearch"
+                       :args (:count 5 :query "Hamlet 1996 reviews")))
+         (new-args '(:count 10 :query "Some other movie")))
+    (gptel--inject-tool-args backend (plist-get testinfo :data) tool-call new-args)
+    (should (equal (map-nested-elt testinfo '(:data :messages 2 :tool_calls 0 :function :arguments))
+                   (gptel--json-encode new-args))))
+
+  (let* ((backend (alist-get 'anthropic gptel-test-backends))
+         (testinfo
+          `( :backend ,backend
+             :data (:messages
+                    [( :role "user"
+                       :content [(:type "text" :text
+                                        "what are the reviews like for Hamlet (1996)?"
+                                        :cache_control (:type "ephemeral"))])
+                     ( :role "assistant"
+                       :content [(:type "text" :text
+                                        "I'll search for reviews of Hamlet (1996) for you.")
+                                 (:type "tool_use" :id "toolu_01J1ZgM54cxXMsEwQn8zMJxH" :name
+                                        "WebSearch" :input (:query "Hamlet 1996 reviews"))])])))
+         (tool-call '( :id "toolu_01J1ZgM54cxXMsEwQn8zMJxH" :name "WebSearch" :input nil
+                       :args (:query "Hamlet 1996 reviews")))
+         (new-args '(:query "Some other moview")))
+    (gptel--inject-tool-args backend (plist-get testinfo :data) tool-call new-args)
+    (should (eq (map-nested-elt testinfo '(:data :messages 1 :content 1 :input))
+                new-args)))
+
+  (let* ((backend (alist-get 'gemini gptel-test-backends))
+         (testinfo
+          `( :backend ,backend
+             :data (:contents
+                    [( :role "user" :parts
+                       [(:text "what are the reviews like for Hamlet (1996)?")])
+                     ( :role "model" :parts
+                       [(:text "")
+                        (:functionCall
+                         (:name "WebSearch" :args
+                                (:query "Hamlet movie 1996 reviews critical reception"))
+                         :thoughtSignature "EtEDCs4DAb4")])])))
+         (tool-call '( :name "WebSearch"
+                       :args (:query "Hamlet movie 1996 reviews critical reception")))
+         (new-args '(:query "Some other movie")))
+    (gptel--inject-tool-args backend (plist-get testinfo :data) tool-call new-args)
+    (should (eq (map-nested-elt testinfo '(:data :contents 1 :parts 1 :functionCall :args))
+                new-args)))
+
+  (let* ((backend (alist-get 'ollama gptel-test-backends))
+         (testinfo
+          `( :backend ,backend
+             :model "qwen3"
+             :data
+             (:messages
+              [( :role "user"
+                 :content "What are the current weather conditions and temperature in New York and London?")
+               ( :role "assistant"
+                 :tool_calls [( :type "function"
+                                :function ( :index 0 :name "get_temperature"
+                                            :arguments (:city "New York")))
+                              ( :type "function"
+                                :function ( :index 1 :name "get_conditions"
+                                            :arguments (:city "New York")))
+                              ( :type "function"
+                                :function ( :index 2 :name "get_temperature"
+                                            :arguments (:city "London")))
+                              ( :type "function"
+                                :function ( :index 3 :name "get_conditions"
+                                            :arguments (:city "London")))])]
+              :stream :json-false)))
+         (tool-call '( :name "get_temperature" :args (:city "London")))
+         (new-args '(:city "Khartoum")))
+    (gptel--inject-tool-args backend (plist-get testinfo :data) tool-call new-args)
+    (should (eq (map-nested-elt testinfo '(:data :messages 1 :tool_calls 2 :function :arguments))
+                new-args))))
+
+;; ;; Ollama sample messages array
+;; ( :model "qwen3"
+;;   :messages
+;;   [( :role "user"
+;;      :content "What are the current weather conditions and temperature in New York and London?")
+;;    ( :role "assistant"
+;;      :tool_calls [( :type "function"
+;;                     :function ( :index 0 :name "get_temperature"
+;;                                 :arguments (:city "New York")))
+;;                   ( :type "function"
+;;                     :function ( :index 1 :name "get_conditions"
+;;                                 :arguments (:city "New York")))
+;;                   ( :type "function"
+;;                     :function ( :index 2 :name "get_temperature"
+;;                                 :arguments (:city "London")))
+;;                   ( :type "function"
+;;                     :function ( :index 3 :name "get_conditions"
+;;                                 :arguments (:city "London")))])
+;;    ( :role "tool" :tool_name "get_temperature" :content "22°C")
+;;    ( :role "tool" :tool_name "get_conditions" :content "Partly cloudy")
+;;    ( :role "tool" :tool_name "get_temperature" :content "15°C")
+;;    ( :role "tool" :tool_name "get_conditions" :content "Rainy")]
+;;   :stream :json-false)
+
 ;; Read an API output stream and construct the response and tool calls
 
 ;;; General
